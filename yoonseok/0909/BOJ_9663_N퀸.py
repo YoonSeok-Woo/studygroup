613b8c0e85f0132d90427ab6c925ca82aa81262c
YoonSeok Woo <wysf1994@naver.com>
2021-09-04 14:46:13 +0900

    0904

---
+++ yoonseok/0909/BOJ_9663_N퀸.py
@@ -0,0 +1,22 @@
+N = int(input())
+q_loc = [-1]*N
+count = 0
+def dfs(ind):
+    global count
+    if ind == N:
+        count+=1
+        return
+    for i in range(N):
+        for k in range(ind):
+            if i == q_loc[k]:
+                break
+            if abs(i-q_loc[k])==abs(k-ind):
+                break
+        else:
+            if ind+1==N:
+                count+=1
+                return
+            q_loc[ind]=i
+            dfs(ind+1)
+dfs(0)
+print(count)
